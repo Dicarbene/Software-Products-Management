23049fe55c705219c648cc1e18b287e64bc95482
Dicarbene <1041641731@qq.com>
2022-04-27 16:14:22 +0800

    新建与组件优化

---
+++ src/components/Buttons/upDown.vue
@@ -0,0 +1,24 @@
+<script setup>
+  const backToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+  const backToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth'
+    })
+  }
+</script>
+<template>
+  <ul class="menu bg-base-100 w-24 rounded-box">
+    <li>
+      <a @click="backToTop()">Top</a>
+    </li>
+    <li>
+      <a @click="backToBottom()">Bottom</a>
+    </li>
+  </ul>
+</template>
--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,56 @@
-
 const Home = () => import("./views/Home.vue");
 const About = () => import("./views/About.vue");
 const NotFound = () => import("./views/NotFound.vue");
 const testView = () => import("./components/testView.vue");
 const Board = () => import("./views/Board.vue");
-
+const User = () => import("./views/User.vue");
+const RepoBoard = () => import("./views/RepositoryBoard.vue");
+const Repo = () => import("./views/Repository.vue");
+const Code = () => import("./views/SingleCode.vue");
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
-  { path: "/", component: Home, meta: { title: "Home" } },
-  {
+  { //首页
+    path: "/",
+    component: Home,
+    meta: { title: "Home" } 
+  },
+  { //关于页
     path: "/about",
     meta: { title: "About" },
     component: About,
-    // example of route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import('./views/About.vue')
   },
-  {
+  { //用户总版
+    path: "/users",
+    component: Board,
+    meta: { title: "Users Board" },
+  },
+  { //用户页
+    path: "/user/:id",
+    component: User,
+    meta: { title: "User" },
+  },
+  { //仓库总版
+    path: "/repos",
+    component: RepoBoard,
+    meta: { title: "Repository Board" },
+  },
+  { //仓库页
+    path: "/repo/:id",
+    component: Repo,
+    meta: { title: "Repository" },
+  },
+  { //代码页
+    path: "/code/:id",
+    component: Code,
+    meta: { title: "Code" },
+  },
+  { //404页
+    path: "/:path(.*)",
+    component: NotFound 
+  },
+  { //测试页
     path: "/testview",
     component: testView,
     meta: { title: "testView" },
   },
-  {
-    path: "/board",
-    component: Board,
-    meta: { title: "Post Board" },
-  },
-  { path: "/:path(.*)", component: NotFound },
 ];
