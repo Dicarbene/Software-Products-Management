fa47e1f10cbda5938644ae3660dd296ea5785c34
Dicarbene <1041641731@qq.com>
2022-05-01 00:23:44 +0800

    初版用户总版

--- src/components/Cards/userCard.vue
+++ src/components/Cards/userCard.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+let user = ref({});
+fetch('https://fakestoreapi.com/users/1')
+  .then(res => res.json())
+  .then(json => { user.value = json })
 </script>
 
 <template>
-  <article class = "card card-compact">
-    <img class="mask mask-hexagon" src="https://api.lorem.space/image/shoes?w=160&h=160" />
-  </article>
+  <div class="flex flex-row flex-wrap">
+    <div class="card card-compact card-side bg-base-100 shadow-xl m-2">
+      <figure><img class="mask mask-hexagon" src="https://api.lorem.space/image/face?w=80&h=80" /></figure>
+      <div class="card-body max-w-sm min-w-sm p-2">
+        <h2 class="card-title">{{ user.username }}</h2>
+        <p class="wrap">{{ JSON.stringify(user.name) }}</p>
+      </div>
+    </div>
+  </div>
 </template>
